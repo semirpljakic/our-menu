79299040e5584d30d6d854e5d8362c8804722503
Semir Pljakić <93542041+semirpljakic@users.noreply.github.com>
2023-01-09 07:49:36 +0100

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -11,7 +11,7 @@ const Menu = ({ items }) => {
             <div className='item-info'>
               <header>
                 <h4>{title}</h4>
-                <h4 className='price'>{price}</h4>
+                <h4 className='price'>${price}</h4>
               </header>
               <p className='item-text'>{desc}</p>
             </div>
